433e8303418335015462eed4ae3e13735071f65d
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-09-22 20:44:40 +0330

    feat: add lessson dynamic route

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="">
+        <h2>Lessons</h2>
+        <p>This is a lesson</p>
+        <p>{{ $route.params.chapterSlug }}</p>
+        <p>{{ $route.params.lessonSlug }}</p>
+    </div>
+</template>
+
