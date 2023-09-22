94673304fcda9bba64365692bb5baaf2adf97118
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-09-22 21:23:17 +0330

    feat: add video player component

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <iframe width="560" height="315" :src="`https://player.vimeo.com/video/${props.videoId}`" title="Video Player"
+    frameborder="0" allow="accelerometer; clipboard-write; encrypted-media;" allowfullscreen></iframe>
+</template>
+
