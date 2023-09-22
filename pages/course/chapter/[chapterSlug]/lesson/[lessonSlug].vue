b0168520958bcd45fc01795981e1bc709c67fefa
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@outlook.com>
2023-09-22 20:59:57 +0330

    feat: read founded route id in course data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -2,8 +2,22 @@
     <div class="">
         <h2>Lessons</h2>
         <p>This is a lesson</p>
-        <p>{{ $route.params.chapterSlug }}</p>
-        <p>{{ $route.params.lessonSlug }}</p>
+        <p>{{ chapter?.title }}</p>
+        <p>{{ lesson?.title }}</p>
     </div>
 </template>
 
+
+<script setup lang="ts">
+const course = useCourse()
+const route = useRoute()
+
+const chapter = computed(() => {
+    return course.chapters.find((chapter) => chapter.slug === route.params.chapterSlug)
+})
+
+const lesson = computed(() => {
+    return chapter.value?.lessons.find((lesson) => lesson.slug === route.params.lessonSlug)
+})
+
+</script>
